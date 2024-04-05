ddb262a93949fee93efee4a9551ed0fb7726e58b
JahongirBozorov <128155644+JahongirBozorov@users.noreply.github.com>
2024-04-05 17:37:42 +0500

    10 dars

--- main.py
+++ main.py
@@ -1,54 +1,34 @@
 # 1-misol
-import collections
-from idlelib.tree import TreeNode
-from typing import Optional
+# def smallestEvenMultiple(n: int) -> int:
+#     return 2 * n if n % 2 == 1 else n
+# print(smallestEvenMultiple(13))
+from typing import List
 
 
-def isBalanced(root: Optional[TreeNode]) -> bool:
-    if root is None:
-        return True
-
-    def height(root):
-        if not root:
-            return 0
-        return 1 + max(height(root.left), height(root.right))
-
-    left, right = height(root.left), height(root.right)
-    if abs(left - right) <= 1:
-        return True and isBalanced(root.left) and isBalanced(root.right)
-    return False
-
-# bu misolda yechimni leetcode yechildi treeni bu yerda qanday yozishni bilmadim
+# oson matematik logika. Kommentga olmasam o'zgaruvhilar va funksiyalarda xatolik chalkashlik bo'lishi mumkin
+# 2-masala
+# def sortPeople(names: List[str], heights: List[int]) -> List[str]:
+#     dict_hn = {h: n for h, n in zip(heights, names)}
+#     heights.sort(reverse=True)
+#     return [dict_hn[h] for h in heights]
+#
+# print(sortPeople(["Mary","John","Emma"], [180,165,170]))
 
 
-# 2-misol
+class LUPrefix:
 
-def minDepth(root: Optional[TreeNode]) -> int:
-    if not root:
-        return 0
-    if not root.left and not root.right:
-        return 1
-    left = minDepth(root.left) if root.left else float('inf')
-    right = minDepth(root.right) if root.right else float('inf')
-    return 1 + min(left, right)
+    def __init__(self, n):
+        self.data = [0]*n
+        self.res = 0
 
-# 1 va 2 - misolda funksiya chaqirilganda oldiga self. qo'ying leetcodda tekshirayotganda
+    def upload(self, video):
+        self.data[video-1] = 1
 
-# 3-misol
+    def longest(self):
+        while self.res < len(self.data) and self.data[self.res] == 1:
+            self.res += 1
 
-def hasPathSum(root: Optional[TreeNode], targetSum: int) -> bool:
-    if not root:
-        return False
-    queue = collections.deque([(root, root.val)])
-    while queue:
-        node, val = queue.popleft()
-        if not node.left and not node.right:
-            if val == targetSum:
-                return True
-            else:
-                continue
-        if node.left:
-            queue.append((node.left, val + node.left.val))
-        if node.right:
-            queue.append((node.right, val + node.right.val))
-    return False
+        return self.res
+
+
+
